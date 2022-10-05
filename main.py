f7826ba00c38e5460254648d5cc6d49216d034fa
GustavSkou <55804968+GustavSkou@users.noreply.github.com>
2022-10-06 00:05:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from DiceRoller import DiceThrow
 
+def check_times_in_row(num):
+    in_row.append(num)
+    if len(in_row) < 2:
+        pass
+    else:
+        for item in in_row:
+            next_num = in_row.index(item) + 1
+            if next_num > len(in_row)-1:
+                break
+            if item == in_row[next_num]:
+                times_in_row.append(1)
+                in_row.pop(in_row.index(item))
+            else:
+                in_row.pop(in_row.index(item))
+
+
+
+ones = []
+twos = []
+trees = []
+fours = []
+fives = []
+sixes = []
+
 fig, ax = plt.subplots()
 
 side = input("how many sides does your dice have? ")
@@ -20,17 +44,15 @@ D.roll(
 D.throw_dices()
 
 print(DiceThrow.lis_throws)
-DiceThrow.lis_throws.sort()
-print(DiceThrow.lis_throws)
+#DiceThrow.lis_throws.sort()
+# print(lis_throws_sorted)
 
-ones = []
-twos = []
-trees = []
-fours = []
-fives = []
-sixes = []
+in_row = []
+times_in_row = []
 
 for x in DiceThrow.lis_throws:
+    check_times_in_row(x)
+
     if x == 1:
         ones.append(x)
     elif x == 2:
@@ -44,19 +66,27 @@ for x in DiceThrow.lis_throws:
     elif x == 6:
         sixes.append(x)
 
-data = [[1, len(ones), len(ones) / len(DiceThrow.lis_throws)], [2, len(twos), len(twos) / len(DiceThrow.lis_throws)], [3, len(trees), len(trees) / len(DiceThrow.lis_throws)], [4, len(fours), len(fours) / len(DiceThrow.lis_throws)], [5, len(fives), len(fives) / len(DiceThrow.lis_throws)], [6, len(sixes), len(sixes) / len(DiceThrow.lis_throws)]]
+data = [[1, len(ones), len(ones) / len(DiceThrow.lis_throws)],
+        [2, len(twos), len(twos) / len(DiceThrow.lis_throws)],
+        [3, len(trees), len(trees) / len(DiceThrow.lis_throws)],
+        [4, len(fours), len(fours) / len(DiceThrow.lis_throws)],
+        [5, len(fives), len(fives) / len(DiceThrow.lis_throws)],
+        [6, len(sixes), len(sixes) / len(DiceThrow.lis_throws)]]
+
 data_count = []
 data_labels = []
 
-for n in range(5)
-    data_count.append([0+n][1])
-    data_labels.append([0+n][0])
+for n in range(6):     # [list][item]
+    data_count.append(data[0+n][1])
+    data_labels.append(data[0+n][0])
 
-print(data)
+# print(data)
+# print(data_count)
+# print(data_labels)
 
 df = pd.DataFrame(data, columns=['side', 'count', 'pro'])
 print(df)
-
+print(len(times_in_row))
 fig1, ax1 = plt.subplots()
 ax1.pie(data_count, labels=data_labels, autopct='%1.1f%%', shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
